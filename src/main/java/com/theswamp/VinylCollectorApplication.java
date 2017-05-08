b968e0debb2752e565ae7f75eb23719d1573c0a6
RedStarFelling <trevgraham@gmail.com>
2017-05-08 23:14:53 +0100

    First commit

---
+++ src/main/java/com/theswamp/VinylCollectorApplication.java
@@ -0,0 +1,12 @@
+package com.theswamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylCollectorApplication.class, args);
+	}
+}
